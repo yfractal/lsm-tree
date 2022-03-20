7f8e07a266cd50e645723db986be0cde035fcbe4
Mike Yang <yfractal@gmail.com>
2022-03-20 21:30:04 +0800

    init

---
+++ spec/lsm_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Lsm do
+  it "has a version number" do
+    expect(Lsm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
